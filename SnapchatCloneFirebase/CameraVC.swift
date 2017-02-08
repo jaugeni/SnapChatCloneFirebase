5f96529b6dc8df99bd3085db040802d6d99cc7b0
jaugeni <jaugeni@gmail.com>
2017-02-08 15:01:10 -0500

    Implement apple example code, Update code to current sintax

---
+++ SnapchatCloneFirebase/CameraVC.swift
@@ -0,0 +1,23 @@
+//
+//  CameraVC.swift
+//  SnapchatCloneFirebase
+//
+//  Created by YAUHENI IVANIUK on 1/20/17.
+//  Copyright © 2017 YauheniIvaniuk. All rights reserved.
+//
+
+import UIKit
+
+class CameraVC: CameraViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+    
+    
+    
+    
+}
+
